0509816e5d791f96bc2174714510035cbc9d4d7f
jaga07u <pradha02jagadish@gmail.com>
2024-12-24 10:13:05 +0530

    debug

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app=express();
 //     res.send("Hello World!")
 // });
 const corsOptions = {
-    origin: "https://spirit-spark.vercel.app", // Replace with your client's origin
+    origin: "http://localhost:3000", // Replace with your client's origin http://localhost:3000  ||https://spirit-spark.vercel.app
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
