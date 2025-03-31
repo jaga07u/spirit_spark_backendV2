48ecce66330ef8e5cf160f73c81d1d065b9b032e
jaga07u <pradha02jagadish@gmail.com>
2025-03-31 09:06:47 +0530

    deletePost added

--- src/app.js
+++ src/app.js
@@ -12,13 +12,12 @@ const app=express();
 //     res.send("Hello World!")
 // });
 const corsOptions = {
-    //origin:"http://localhost:3000",
+  //  origin:"http://localhost:3000",
     origin: "https://spirit-spark-nextjs.vercel.app", // Replace with your client's origin http://localhost:3000  ||https://spirit-spark.vercel.app
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
-
 app.use(cors(corsOptions));
 app.use(express.json({limit:"76kb"}))
 app.use(express.urlencoded({extended:true ,limit:"16kb"}))
