109e06316f2e5f7331f18ba2e51337053e26918b
jaga07u <pradhan02jagadish@gmail.com>
2025-06-20 09:39:18 +0530

    solve import issue

--- src/middleware/auth.midlleware.js
+++ src/middleware/auth.midlleware.js
@@ -1,3 +1,5 @@
+import {ApiError} from "../utils/ApiError.js";
+
 export const verifyJWT = async (req, _, next) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
