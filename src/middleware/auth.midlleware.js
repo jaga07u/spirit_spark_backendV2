81c446d703e1816db44095e72dd3d64a4d51640d
jaga07u <pradhan02jagadish@gmail.com>
2025-06-20 11:24:16 +0530

    Error solved

--- src/middleware/auth.midlleware.js
+++ src/middleware/auth.midlleware.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
-import User from "../models/User.model.js";
-import ApiError from "../utils/ApiError.js";
+import {User} from "../models/User.model.js";
+import {ApiError} from "../utils/ApiError.js";
 
 export const verifyJWT = async(req, res, next) => {
     try {
