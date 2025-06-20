a1d5cc2c0c8f2f1790e48f3fbc1429cc082e40c2
jaga07u <pradhan02jagadish@gmail.com>
2025-06-20 09:20:52 +0530

    Adding new Changes

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -30,6 +30,6 @@ const UploadOnCloudnary=async (localFilePath)=>{
 // cloudinary.v2.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
 //   { public_id: "olympic_flag" }, 
 //   function(error, result) {console.log(result); });
-
+//Add changes
 
   export {UploadOnCloudnary}
